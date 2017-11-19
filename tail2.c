9b221bc14aea36c4876c4821d6beca0f01a48e8c
katsolenkova888 <kat.solenkova.tues19@gmail.com>
2017-11-19 18:12:15 +0200

    stdintry

--- tail2.c
+++ tail2.c
@@ -17,6 +17,7 @@ int main(int argc, char *argv[]){
 	char close_buff[buff_size];
 	char buff[size];
 	char buff2[size];
+	char buff3[size];
 	int cclose;
 	
 	
@@ -29,21 +30,28 @@ int main(int argc, char *argv[]){
 		int count = -1;
 		int line_count = 0;
 		if(*argv[i] == '-' || argc == 1){
+			//char *file = "new_file.txt";
+			//fd = open("new_file.txt", O_CREAT|O_WRONLY,S_IRWXU);
+			while(rres != EOF){
+				rres = read(STDIN_FILENO, buff3, 1);
+				if(rres<0){
+					//int err_read = snprintf(read_buff, buff_size, "tail: cannot open '%s' for reading: No such file or directory\n", argv[i]);
+					//write(STDOUT_FILENO, read_buff, err_read);
+					return -1;
+					
+				}
+				if(rres == 0){
+					break;
+				}
+				
 			
-			return -1;
-		}
-		else{
-			fd = open(argv[i], O_RDONLY);
-			if(fd<0){ 
-				int err_open = snprintf(open_buff, buff_size, "tail: cannot open '%s' for reading: No such file or directory\n", argv[i]);
-				write(STDOUT_FILENO, open_buff, err_open);
-				return -1;
+				
 			}
+			fd = open(buff3, O_RDONLY);
 			if(argc>2){
 				int name = snprintf(write_buff, buff_size, "==> %s <==\n", argv[i]);
 				write(STDOUT_FILENO,write_buff,name);
 			}
-			//offset_end = lseek(fd, 0, SEEK_END);
 			while(1){
 				offset = lseek(fd, count, SEEK_END);
 				if(offset<0){
@@ -75,19 +83,78 @@ int main(int argc, char *argv[]){
 				wres=write(STDOUT_FILENO,buff,rres);
 				if(wres<0){
 					perror("write");
-					return -1;
+					//return -1;
 				} 				
 				
 			}
 			if(i<argc-1){
 				write(STDOUT_FILENO, "\n", 1);
 			}		
-		cclose = close(fd);
-				if(cclose<0){
+			cclose = close(fd);
+			if(cclose<0){
 				int err_close = snprintf(close_buff, buff_size, "tail: error reading '%s': Input/output error\n", argv[i]);
 				write(STDOUT_FILENO, close_buff, err_close);
-				return -1;
+				//return -1;
+			}
+			
+		
+		}
+		else{
+			fd = open(argv[i], O_RDONLY);
+			if(fd<0){ 
+				int err_open = snprintf(open_buff, buff_size, "tail: cannot open '%s' for reading: No such file or directory\n", argv[i]);
+				write(STDOUT_FILENO, open_buff, err_open);
+				//return -1;
+			}
+			if(argc>2){
+				int name = snprintf(write_buff, buff_size, "==> %s <==\n", argv[i]);
+				write(STDOUT_FILENO,write_buff,name);
+			}
+			
+			while(1){
+				offset = lseek(fd, count, SEEK_END);
+				if(offset<0){
+					break;	
 				}
+				count--;
+				rres = read(fd,buff,1);
+				if(rres<0){
+					int err_read = snprintf(read_buff, buff_size, "tail: cannot open '%s' for reading: No such file or directory\n", argv[i]);
+					write(STDOUT_FILENO, read_buff, err_read);
+					return -1;
+					
+				}
+				if(rres == 0){
+					break;
+				}
+				if (line_count == 11){
+					break;
+				}
+				if(buff[0] =='\n'){
+					line_count++;
+				}
+			}
+			if(line_count<11){
+				lseek(fd, 0, SEEK_SET);
+			}
+			else lseek(fd, offset + 2, SEEK_SET);
+			while(rres = read(fd, buff, 1) > 0){
+				wres=write(STDOUT_FILENO,buff,rres);
+				if(wres<0){
+					perror("write");
+					//return -1;
+				} 				
+				
+			}
+			if(i<argc-1){
+				write(STDOUT_FILENO, "\n", 1);
+			}		
+			cclose = close(fd);
+			if(cclose<0){
+				int err_close = snprintf(close_buff, buff_size, "tail: error reading '%s': Input/output error\n", argv[i]);
+				write(STDOUT_FILENO, close_buff, err_close);
+				//return -1;
+			}
 		}
 	
 	}
